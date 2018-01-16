f80b5c66704d03b4de11e8c89fdaf423c1b92985
Danco90 <danielematteo90@gmail.com>
2018-01-16 14:55:04 +0100

    added jdbc example project

---
+++ my-jdbc-example/src/main/java/com/daniele/project/App.java
@@ -0,0 +1,13 @@
+package com.daniele.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
